4de34d74f7dcdd5842c036b9f0ce20fcb923c070
Evan Bruhn <evan.bruhn@gmail.com>
2013-12-31 20:37:45 +1100

    Added support for storage quota checking, index number tracking by DB and maximum age of torrent

--- README.md
+++ README.md
@@ -7,6 +7,14 @@
 >
 >Usage: `$ python yoink.py`
 >
+>This is a forked version of tobbez's what.cd grabber, with some additions:
+>* Added some new features, represented by the below parameters:
+>	* max_age: The maximum age in days that freeleech torrents should be downloaded. This was cherrypicked from [bitfoo's commit](https://github.com/bitfoo/yoink/commit/2b980191c80f8c17b83235fcd431ed7af53af4e8).
+>	* max_storage_in_mb: Applicable to seedboxes with limited storage, the maximum capacity (in MB) of your storage area. If left blank, the size of your storage area will not be evaluated.
+>	* storage_dir: The folder that will be evaluated when checking if the maximum capacity of your storage area has been exceeded. Defaults to your home directory.
+>	* track_by_index_number: If TRUE, will track and check torrent IDs in a sqlite db (~/.yoink.db) in addition to checking for the torrent file in your watch directory.
+>* Changed the sleep between yoinks to occur after each API request, rather than each torrent download.
+>
 >On the initial run, a file called `~/.yoinkrc` will be generated.
 >
 >Edit this file to include the proper information, then re-run `$ python yoink.py`
@@ -20,6 +28,7 @@
 >`00 * * * * python /path/to/yoink.py`
 
 
-**Now work out that buffer!**
+**Now work out that buffer! (without blowing your storage quota)**
 
 *<3 [tobbez](https://what.cd/user.php?id=605)*
+*<3 [evanjd/notarebel](https://what.cd/user.php?id=417)*
--- yoink.py
+++ yoink.py
@@ -9,27 +9,86 @@ import os
 import re
 import sys
 import argparse
+import sqlite3
 from os.path import expanduser
+
+## SAFE TO EDIT ##
+dbpath = '~/.yoink.db'
 
 ## DO NOT TOUCH THESE ##
 user = ''
 password = ''
 target = ''
+max_storage = ''
+max_age = ''
+storage_dir = ''
+track_by_index_number = None
 
-defaultrc=["user:",'\n',"password:",'\n',"target:"]
+defaultrc=["user:",'\n',"password:",'\n',"target:"'\n',"max_age:"'\n',"max_storage_in_mb:"'\n',"storage_dir:"'\n',"track_by_index_number:TRUE"]
 
 headers = {
   'User-Agent': 'Yoink! Beta'
 }
+
+def isStorageFull(max_storage):
+  if max_storage == False:
+    return False
+
+  totalSize = sum( os.path.getsize(os.path.join(dirpath,filename)) for dirpath, dirnames, filenames in os.walk( storage_dir ) for filename in filenames ) /1024/1024
+  if totalSize >= max_storage:
+    return True
+  else:
+    return False
+
+def torrentAlreadyDownloaded(tid):
+  if track_by_index_number:
+    try:
+      indexdb = sqlite3.connect(os.path.expanduser(dbpath))
+      indexdbc = indexdb.cursor()
+      indexdbc.execute("SELECT COUNT(*) FROM snatchedtorrents WHERE torrent_id = (?)", [tid])
+      if int(str(indexdbc.fetchone())[1]) == 0:
+        torrent_found = False
+      else:
+        torrent_found = True
+    except Exception,e:
+      print 'Error when executing SELECT on ~/.yoink.db:'
+      print str(e)
+      sys.exit()
+    finally:
+      if indexdb:
+        indexdbc.close()
+      return torrent_found
+  else:
+    return False
+
+def addTorrentToDB(tid):
+  if track_by_index_number:
+    try:
+      indexdb = sqlite3.connect(os.path.expanduser(dbpath))
+      indexdbc = indexdb.cursor()
+      indexdbc.execute("INSERT INTO snatchedtorrents values (?)", [tid])
+      indexdb.commit()
+    except Exception,e:
+      print 'Error when executing INSERT on ~/.yoink.db:'
+      print str(e)
+      sys.exit()
+    finally:
+      if indexdb:
+        indexdbc.close()
 
 def download_torrent(session, tid, name):
   if not os.path.exists(target):
     print 'Target Directory does not exist, creating...'
     os.mkdir(target)
 
+  if torrentAlreadyDownloaded(tid):
+    print 'I have previously downloaded {}.'.format(tid)
+    return
+
   path = os.path.join(target, name)
   if os.path.exists(path):
     print 'I already haz {}.'.format(tid)
+    addTorrentToDB(tid)
     return
 
   if not hasattr(download_torrent, 'authdata'):
@@ -42,7 +101,7 @@ def download_torrent(session, tid, name):
   with open(path, 'wb') as f:
     for chunk in dl.iter_content(1024*1024):
       f.write(chunk)
-  time.sleep(2)
+  addTorrentToDB(tid)
   print 'Yoink!'
 
 def main():
@@ -58,6 +117,10 @@ def main():
     print 'user:USERNAME'
     print 'password:PASSWORD'
     print 'target:TORRENTDIR'
+    print 'max_age:NUMDAYS [Optional, default: 3650]'
+    print 'max_storage_in_mb:NUMINMEGABYTES [Optional, if left blank will not check]'
+    print 'storage_dir:STORAGEDIR [Optional, default home directory]'
+    print 'track_by_index_number:TRUE or FALSE'
     return 0
   else:
     rcf = open(rcpath)
@@ -67,9 +130,60 @@ def main():
     password = rcf.readline().rstrip('\n')[9:]
     global target
     target = os.path.expanduser(rcf.readline().rstrip('\n')[7:])
-    if user=='' or password=='' or target=='':
+    global max_age
+    max_age = rcf.readline().rstrip('\n')[8:]
+    global max_storage
+    max_storage = rcf.readline().rstrip('\n')[18:]
+    global storage_dir
+    storage_dir = rcf.readline().rstrip('\n')[12:]
+    global track_by_index_number
+    track_by_index_number = rcf.readline().rstrip('\n')[22:]
+    
+    if user=='' or password=='' or target=='' or track_by_index_number=='':
       print 'Finish filling out ~/.yoinkrc and try again!'
       return 0
+
+    if max_age != '' and max_age.isdigit() == False:
+      print 'Max Age (max_age) parameter must be a whole positive number.'
+      return 0
+    elif max_age == '':
+      max_age = 3650
+    else:
+      max_age = int(max_age)
+
+    if max_storage != '' and max_storage.isdigit() == False:
+      print 'Max Storage (max_storage) parameter must be a whole positive number.'
+      return 0
+    elif max_storage == '':
+      max_storage = False
+    else:
+      max_storage = int(max_storage)
+
+    if storage_dir != '':
+      try:
+        storage_dir = os.path.expanduser(storage_dir)
+        if os.path.exists(storage_dir) == False:
+          raise NameError('InvalidPath')
+      except:
+        print 'Storage directory (storage_dir) paramater does not resolve to a known directory.'
+        return 0
+    else:
+      storage_dir = os.path.expanduser('~')
+
+    if track_by_index_number.upper() == 'TRUE':
+      track_by_index_number = True
+      if os.path.exists(os.path.expanduser(dbpath)) == False:
+        open(os.path.expanduser(dbpath), 'w+').close()
+      indexdb = sqlite3.connect(os.path.expanduser(dbpath))
+      indexdbc = indexdb.cursor()
+      indexdbc.execute("CREATE TABLE IF NOT EXISTS snatchedtorrents (torrent_id NUMBER(100))")
+      indexdb.commit()
+    elif track_by_index_number.upper() == 'FALSE':
+      track_by_index_number = False
+    else:
+      print 'Track by index number (track_by_index_number) parameter must be TRUE or FALSE.'
+      return 0
+
   search_params = 'search=&freetorrent=1'
 
   html_parser = HTMLParser.HTMLParser()
@@ -93,11 +207,22 @@ def main():
   with open(cookiefile, 'w') as f:
     pickle.dump(s.cookies, f)
 
+  cur_time = int(time.time())
+  oldest_time = cur_time - (int(max_age) * (24 * 60 * 60))
+
+  continueLeeching = True
   page = 1
-  while True:
+  while continueLeeching:
     r = s.get('https://what.cd/ajax.php?action=browse&' + search_params + "&page={}".format(page), headers=headers)
     data = json.loads(r.content)
     for group in data['response']['results']:
+      if int(group['groupTime']) < oldest_time:
+        continueLeeching = False
+        break
+      if isStorageFull(max_storage):
+        continueLeeching = False
+        print 'Your storage equals or exceeds ' + str(max_storage) + 'MB, exiting...'
+        break
       if 'torrents' in group:
         for torrent in group['torrents']:
           if not torrent['isFreeleech']:
@@ -107,7 +232,7 @@ def main():
       else:
         fn = clean_fn('{} {}.torrent'.format(group['torrentId'], group['groupName'][:100]))
         download_torrent(s, group['torrentId'], fn)
-
+      time.sleep(2)
     page += 1
     if page > data['response']['pages']:
       print "Wow, done already?"
